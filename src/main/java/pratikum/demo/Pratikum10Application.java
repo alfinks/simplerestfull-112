0278eae3760eb1cf391f907748afc17e0b1a9ae9
alfinks <maulanaafiansyah@gmail.com>
2022-12-05 17:18:28 +0700

    membuat project spring boot

---
+++ src/main/java/pratikum/demo/Pratikum10Application.java
@@ -0,0 +1,13 @@
+package pratikum.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pratikum10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pratikum10Application.class, args);
+	}
+
+}
---
+++ src/test/java/pratikum/demo/Pratikum10ApplicationTests.java
@@ -0,0 +1,13 @@
+package pratikum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pratikum10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
